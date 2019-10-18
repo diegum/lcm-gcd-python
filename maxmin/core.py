3b0090c19caf5b4f821e8807db9b429d0bb77235
Diego Dagum <diego@diegodagum.com>
2019-10-18 12:41:33 -0700

    Prime factorization.

--- maxmin/core.py
+++ maxmin/core.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 """LCM, GCD module."""
 
-
-from typing import Generator
-
+from typing import Generator, Union
 
 from math import ceil, sqrt
 
@@ -35,3 +33,28 @@ def generate_primes_under(number: int) -> Generator[int, None, None]:
         if is_prime(iterator_number):
             yield iterator_number
         iterator_number += (1 if iterator_number == 2 else 2)
+
+
+def get_next_generated_value_or_none(generator: Generator[int, None, None]) -> Union[int, None]:
+    """
+    Returns the next element of an argument generator, or None, if a StopIteration exception arose.
+    """
+    try:
+        return next(generator)
+    except StopIteration:
+        pass
+
+
+def generate_prime_factors(number: int) -> Generator[int, None, None]:
+    """Yields ascending or equal prime factors that compose the argument number."""
+    abs_number = abs(number)
+    primes = generate_primes_under(number + 1)
+
+    last_prime = get_next_generated_value_or_none(primes)
+    if last_prime is not None:
+        while abs_number != 1:
+            if abs_number % last_prime == 0:
+                yield last_prime
+                abs_number = int(abs_number / last_prime)
+            else:
+                last_prime = get_next_generated_value_or_none(primes)
---
+++ tests/step_defs/test_factorization.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Prime factorization tests.
+"""
+
+
+# pylint: disable=C0116, W0621
+
+
+from ast import literal_eval
+from pytest_bdd import given, when, then, scenario
+
+
+from ..context import maxmin
+
+
+@scenario(
+    '../features/prime_factorization.feature',
+    'Prime factorization',
+    example_converters=dict(number=int, prime_factors=str)
+)
+def test_factorization():
+    pass
+
+
+@given('a <number>')
+def number_under_test(number):
+    assert isinstance(number, int)
+    return dict(number=number)
+
+
+@when('submitted to prime factorization')
+def generate_prime_factors(number_under_test):
+    number_under_test['prime_factors'] = maxmin.generate_prime_factors(number_under_test['number'])
+
+
+@then('it generates its <prime_factors>')
+def generated_prime_factors(number_under_test, prime_factors):
+    prime_list = literal_eval(prime_factors)
+    index = 0
+    prime_factor_generator = number_under_test['prime_factors']
+    for factor in prime_factor_generator:
+        assert index < len(prime_list), "Unexpected factors generated"
+        assert factor == prime_list[index], "Generated factor different than expected"
+        index += 1
+    assert index == len(prime_list), "More prime factors expected"
